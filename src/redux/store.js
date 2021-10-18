50a7fc2f492f0b8d3e4a61b7053b0dcf28101af2
Irina Romjalis <iromjalis@gmail.com>
2021-10-18 12:43:21 +0300

    redux-logger + Provider

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { rootReducer } from "./phonebook/phonebook-reducer";
-import logger from "logger";
+import logger from "redux-logger";
 
 const store = configureStore({
   reducer: rootReducer,
   middleware: [...getDefaultMiddleware(), logger],
   devTools: true,
 });
+export default store;
