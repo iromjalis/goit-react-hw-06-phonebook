c1ae181984bb74430a33e20933c3772d4fd31d62
Irina Romjalis <iromjalis@gmail.com>
2021-10-24 16:32:54 +0300

    app.js without state

--- src/App.js
+++ src/App.js
@@ -6,88 +6,49 @@ import ContactList from "./components/ContactList/ContactList";
 import Container from "./components/Container/Container";
 import Filter from "./components/Filter/Filter";
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
-    filter: "",
-    name: "",
-    number: "",
-  };
+function App() {
 
-  componentDidMount() {
-    const contacts = localStorage.getItem("contacts");
-    if (contacts) {
-      this.setState({ contacts: JSON.parse(contacts) });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const { contacts } = this.state;
-    if (contacts !== prevState.contacts) {
-      localStorage.setItem("contacts", JSON.stringify(contacts));
-    }
-  }
-
-  handleSubmit = (data) => {
-    this.setState({ ...data });
-    this.setState({ name: "", number: "" });
-  };
-  handleFilterChange = (e) => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-  handleDeleteContact = (contactId) => {
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.filter(
-        (contact) => contact.id !== contactId
-      ),
-    }));
-  };
-  handleAddContact = (data) => {
-    const newContact = {
-      id: shortid.generate(),
-      ...data,
-    };
-
-    if (this.state.contacts.some(({ name }) => name === data.name)) {
-      alert(`${data.name} already exists`);
-      return;
-    }
-    this.setState(({ contacts }) => ({
-      contacts: [newContact, ...contacts],
-    }));
-    this.setState({ name: "", number: "" });
-  };
-
-  render() {
-    const { name, number, filter, contacts } = this.state;
-    const filtered = contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-    return (
-      <>
-        <Container className="App">
-          <h1>Phonebook</h1>
-          <ContactForm
-            name={name}
-            number={number}
-            handleSubmit={this.handleSubmit}
-            onSubmit={this.handleAddContact}
-          />
-          <h2>Contacts</h2>
-          <Filter value={filter} handleFilterChange={this.handleFilterChange} />
-          <ContactList
-            contacts={filtered}
-            handleDeleteContact={this.handleDeleteContact}
-          />
-        </Container>
-      </>
-    );
-  }
+  return (
+    <>
+      <Container className="App">
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList />
+      </Container>
+    </>
+  );
 }
 
 export default App;
+
+  // handleSubmit = (data) => {
+  //   this.setState({ ...data });
+  //   this.setState({ name: "", number: "" });
+  // };
+  // handleFilterChange = (e) => {
+  //   this.setState({ filter: e.currentTarget.value });
+  // };
+  // handleDeleteContact = (contactId) => {
+  //   this.setState((prevState) => ({
+  //     contacts: prevState.contacts.filter(
+  //       (contact) => contact.id !== contactId
+  //     ),
+  //   }));
+  // };
+  // handleAddContact = (data) => {
+  //   const newContact = {
+  //     id: shortid.generate(),
+  //     ...data,
+  //   };
+
+  //   if (this.state.contacts.some(({ name }) => name === data.name)) {
+  //     alert(`${data.name} already exists`);
+  //     return;
+  //   }
+  //   this.setState(({ contacts }) => ({
+  //     contacts: [newContact, ...contacts],
+  //   }));
+  //   this.setState({ name: "", number: "" });
+  // };
